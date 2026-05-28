5489e2d6a13619a49e121b528c6b2046d0c32f0e
YC <ug000sx4@gmail.com>
2026-05-28 02:24:01 +0200

    Integrate middleware for async actions and logging, refactor quote fetching to use thunk actions (redux-thunk, redux-logger)

--- src/configureStore/store.js
+++ src/configureStore/store.js
@@ -1,10 +1,10 @@
 import { legacy_createStore as createStore } from 'redux';
 import { accountReducer } from "../reducer/accountReducer.js";
 import { applyMiddleware } from "redux";
-import { loggerEnhancer } from "../enhancers/loggerEnhancer.js";
-import { thunkEnhancer } from "../enhancers/thunkEnhancer.js";
+import {thunk} from 'redux-thunk';
+import {logger} from 'redux-logger';
 
 const initialState = { balance: 0, quote: "Winter is coming..." };
 
 export const store = createStore(accountReducer, initialState,
-     applyMiddleware(thunkEnhancer, loggerEnhancer));
+     applyMiddleware(thunk, logger));
